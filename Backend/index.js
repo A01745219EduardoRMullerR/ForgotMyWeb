13db85a1166900e3790aa4fcb1f2acff0cf0f400
Eduardo R. Müller <a01745219@itesm.mx>
2021-11-24 11:55:23 -0600

    Backend iniciado, npm start, librerias express y mongoose, schemas listos

---
+++ Backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.use((req, res, next) =>{
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'POST')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    next()
+
+})
---
+++ Backend/models/materias.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+
+const schemaMaterias = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    nombre:{
+        Type: String,
+        required: true
+    },
+    clave:{
+        Type: String,
+        required: false
+    },
+    tareas: {
+        Type: Schema.ObjectId,
+        required: false
+    }
+})
+
+const schemaTarea = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    nombre:{
+        Type: String,
+        required: true
+    },
+    description: {
+        Type: String,
+        required: true
+    },
+    entrega:{
+        Type: Date,
+        required: false
+    },
+    valor:{
+        Type: Number,
+        required: false
+    }
+
+})
+
+const materias = mongoose.model('Materias', schemaMaterias)
+const tareas = mongoose.model('Tareas', schemaTarea)
+
+module.exports = materias
+module.exports = tareas
