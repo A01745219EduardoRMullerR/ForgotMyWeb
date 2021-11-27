2d2f1521b32e9b078a47137815c92074616eceef
Eduardo R. Müller <a01745219@itesm.mx>
2021-11-26 19:56:50 -0600

    Borrado listo, TODO's en agregar y actualizar

--- Backend/controllers/materias.js
+++ Backend/controllers/materias.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const schemaMaterias = require('../models/materias')
 
 exports.agregarMateria = async (req, res) => {
-
+    //TODO: Decidir estructura de subida
 }
 
 exports.getMaterias = async (req, res) => {
@@ -16,9 +16,16 @@ exports.getMaterias = async (req, res) => {
 }
 
 exports.actualizarMaterias = async(req, res) => {
-
+    //TODO: Ver estructura del JSON para realizar cambios en el esquema
 }
 
 exports.borrarMateria = async (req, res) => {
-
+    try{
+        await schemaMaterias.findOneAndDelete(req.body)
+        res.json({Status: 200})
+        console.log('Elemento borrado con exito')
+    } catch(err){
+        console.log('Error en borrado de elemento (' + err +')')
+        res.json({Status: 500})
+    }
 }
