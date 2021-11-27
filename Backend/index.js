44b77396a6262ddb80f21cb20ee44bb2e5f894ba
Eduardo R. Müller <a01745219@itesm.mx>
2021-11-26 20:09:51 -0600

    index.js actualizado para conectarse a MongoDB

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,30 @@
 const express = require('express')
+const path = require("path");
 const app = express()
+const routeMaterias = require('../Backend/routes/materias')
+const routeTareas = require('../Backend/routes/tareas')
+const mongoose = require("mongoose");
 
-app.use((req, res, next) =>{
+/*app.use((req, res, next) =>{
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'POST')
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
     next()
 
-})
+})*/
+app.use(express.static(path.join(__dirname, 'Backend')))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-app.listen(8080, ()=>{
+app.use('/post', routeMaterias)
+app.use('/post', routeTareas)
+
+mongoose.connect('mongodb://user10:root@54.198.161.35:27017/base10?authSource=admin')
+    .then(()=>{
+        app.listen(8081,()=>console.log("Servidor Back-End en línea"))
+    })
+    .catch(err=>console.log(err))
+
+/*app.listen(8080, ()=>{
     console.log('Backend en linea.... :D')
-})
+})*/
